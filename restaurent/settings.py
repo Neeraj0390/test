2ec1899df6a017ddb2166de7576314ac59ca4f21
Neeraj0390 <pneerajreddy390@gmail.com>
2025-03-24 20:58:28 +0000

    Update settings.py

--- restaurent/settings.py
+++ restaurent/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"  # For production collectstatic
+STATIC_URL = '/static/'  # URL for accessing static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # The directory for collected static files
+
 
 
 
